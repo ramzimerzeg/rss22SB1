6953c8950a53fdd1101518d1dad10022688a4e0d
MERZEG <ameuramzi@gmail.com>
2022-04-27 16:21:11 +0200

    Modification méthode post

--- src/main/java/fr/univrouen/rss22/controllers/HtmlController.java
+++ src/main/java/fr/univrouen/rss22/controllers/HtmlController.java
@@ -7,6 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @Controller
@@ -23,14 +31,21 @@ public class HtmlController {
 
 		return "all-items";
 	}
+	@PostMapping(value = "/insert", produces = MediaType.APPLICATION_XML_VALUE)
+	public String insertItem(@RequestBody Item newItem, Model model) {
+		model.addAttribute("item", newItem);
+		itemRepository.save(newItem);
+		System.out.println("***********************-----------------------\n" + newItem.getGuid());
+		return "insert";
+	}
 
-	@PostMapping(
-			value = "/insert"
-	)
+	/*@ResponseBody
 	public String insertItem(@ModelAttribute Item newItem, Model model) {
 		model.addAttribute("item", newItem);
 		System.out.println("***********************-----------------------\n" + newItem.getGuid());
 		return "insert";
-	}
+	}*/
+
+
 	
 }
