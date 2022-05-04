ef1895de706dba1d3b5fa20a4d7fb6a723e6cb2d
MERZEG <ameuramzi@gmail.com>
2022-05-04 16:43:33 +0200

    fix cross origin

--- src/main/java/fr/univrouen/rss22/controllers/XmlController.java
+++ src/main/java/fr/univrouen/rss22/controllers/XmlController.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class XmlController {
 
